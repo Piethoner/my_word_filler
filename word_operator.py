3ab5e37d3a737e7e1be2f9b530b77b18f8212b1b
MineMine <1464250028@qq.com>
2019-12-18 18:24:33 +0800

    Update word_operator.py

--- word_operator.py
+++ word_operator.py
@@ -4,7 +4,7 @@ import re
 import pythoncom
 import win32com.client as win32
 
-# word中某些字符需要做转换, 使用这个方式（translate用于单字符, replace用于多字符）效率更高
+# word中某些字符需要做转换, 使用这个方式（translate只能用于单字符, replace常用于多字符）
 CHARACTER_MAP = str.maketrans({
         '\r': '\n',
         '\t': ' ',
